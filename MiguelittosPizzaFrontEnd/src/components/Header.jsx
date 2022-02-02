a701b3544001c576c4ebe00f4a095108709ec699
Martin Olson <martinguyolson@gmail.com>
2022-02-02 18:59:27 +1100

    tring to style the header. apparently local image storage and display is impossible

---
+++ MiguelittosPizzaFrontEnd/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+const Header = () => 
+{
+  return(
+    <nav class="navbar navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href='#'>
+          <img src='public/images/miguelittosPizzeriaLogo.jpg' alt="Logo" height={30} width={30}/>
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
