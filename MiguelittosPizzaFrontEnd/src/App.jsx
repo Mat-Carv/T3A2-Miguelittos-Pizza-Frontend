d96c9bbebec4e972e3d31fe3cb90a6d8ef230650
Martin Olson <martinguyolson@gmail.com>
2022-02-07 17:40:15 +1100

    added example fetch to app.jsx which connects to live backend deployment

--- MiguelittosPizzaFrontEnd/src/App.jsx
+++ MiguelittosPizzaFrontEnd/src/App.jsx
@@ -4,10 +4,18 @@ import { useState } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
+  fetch('https://pizzaria-miguel.herokuapp.com/api/products/index/1',{
+    headers:{
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      //Authentication: token
+    }
+  })
 
   return (
     <div className="App">
       <h1>Hello world</h1>
+      
     </div>
   )
 }
