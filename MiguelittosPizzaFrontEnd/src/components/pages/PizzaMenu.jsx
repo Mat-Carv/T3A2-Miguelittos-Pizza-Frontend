c3bf1d83af45c9b4248d3cfc6b667ef729ec2d41
Martin Olson <martinguyolson@gmail.com>
2022-02-08 19:58:48 +1100

    bootstrap styling to index.html, boostrapping PizzaMenu page

--- MiguelittosPizzaFrontEnd/src/components/pages/PizzaMenu.jsx
+++ MiguelittosPizzaFrontEnd/src/components/pages/PizzaMenu.jsx
@@ -1,39 +1,69 @@
 import React, { Component, useEffect, useState } from 'react';
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const PizzaMenu = () => {
 
+  const pizzaMenuUrl = "https://pizzaria-miguel.herokuapp.com/api/pizzas/index";
+  // const ingredientsUrl = "https://pizzaria-miguel.herokuapp.com/api/products/index/1";
+
   const [menuItems, setMenuItems] = useState([])
+  // const [ingredients, setIngredients] = useState([])
+
+  // useEffect(async () => {
+  //   console.log ("ingredients fetched")
+  //   try{
+  //     const {status, data} = await axios.get(ingredientsUrl)
+  //     console.log (status,data)
+  //     setIngredients(data)
+  //   }
+
+  //   catch(error){
+  //     console.error(error + " in ingredients fetch")
+  //   }
+
+  // }, [])
 
   useEffect(async () => {
     console.log("menu fetched")
       try{
-        const {status, data} = await axios.get("https://pizzaria-miguel.herokuapp.com/api/pizzas/index") 
+        const {status, data} = await axios.get(pizzaMenuUrl) 
         console.log (status, data)
         setMenuItems(data)
       }
       catch(error){
-          console.error(error)
+          console.error(error + " in menu fetch")
       }
   }, [])
 
-  const pizzaMenu = menuItems.map((menuItem,index) => {
-     return (
+
+  const pizzaMenu = menuItems.map((menuItem,menuIndex) => {
+     return (//the correct way to do style is below
      <>
-        <div key ={index}>
-        <h3>{menuItem.name}</h3>
-        <h3>{menuItem.base}</h3>
-        <h3>{menuItem.sauce}</h3>
+        <div className="card" style={{width: 18 +'em'}}>
+          <img src="[placeholder]" className='card-img' alt='pizzaimage'></img>
+          <div className="card-img-overlay">
+            <h4 className="card-title">{menuItem.name}</h4>
+            <p className="card-text">{menuItem.description}</p>
+            {/* <h4>{menuItem.base}</h4>
+            <h4>{menuItem.sauce}</h4>
+            <h4>{menuIndex}</h4>  these are all things you can call. not price though*/}
+         </div>
         <p></p>
 
       </div>
-
-     {/* <li key={menuItem.name}>{menuItem.name}</li> */}
+            <p></p>
+      <p></p>
      </>
      )
   })
 
-  return <div>{pizzaMenu}</div>
+  return (
+  <>
+  <div>{pizzaMenu}</div>
+  <p></p><p></p>
+ 
+  </>)
 }
 
 export default PizzaMenu
