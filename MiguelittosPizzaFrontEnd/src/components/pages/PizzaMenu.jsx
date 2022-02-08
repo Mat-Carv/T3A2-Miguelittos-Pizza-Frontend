5bf7103e45dddbc24ca701eff1125327bfd524a1
Martin Olson <martinguyolson@gmail.com>
2022-02-08 20:07:42 +1100

    broken horribly due to lost brace or something

--- MiguelittosPizzaFrontEnd/src/components/pages/PizzaMenu.jsx
+++ MiguelittosPizzaFrontEnd/src/components/pages/PizzaMenu.jsx
@@ -38,80 +38,25 @@ const PizzaMenu = () => {
 
 
   const pizzaMenu = menuItems.map((menuItem,menuIndex) => {
-     return (//the correct way to do style is below
-     <>
-        <div className="card" style={{width: 18 +'em'}}>
-          <img src="[placeholder]" className='card-img' alt='pizzaimage'></img>
-          <div className="card-img-overlay">
-            <h4 className="card-title">{menuItem.name}</h4>
-            <p className="card-text">{menuItem.description}</p>
-            {/* <h4>{menuItem.base}</h4>
-            <h4>{menuItem.sauce}</h4>
-            <h4>{menuIndex}</h4>  these are all things you can call. not price though*/}
+     return (
+       //the correct way to do style is below
+         <>
+         <div className="card" style={{ width: 18 + 'em' }}>
+         <img src="[placeholder]" className='card-img' alt='pizzaimage'></img>
+         <div className="card-img-overlay">
+           <h4 className="card-title">{menuItem.name}</h4>
+           <p className="card-text">{menuItem.description}</p>
          </div>
-        <p></p>
-
-      </div>
-            <p></p>
-      <p></p>
-     </>
-     )
-  })
-
-  return (
-  <>
-  <div>{pizzaMenu}</div>
-  <p></p><p></p>
- 
-  </>)
+          </>
+         )
+         }
+)
+         return(
+         <>
+           <div>{pizzaMenu}</div>
+           <p></p><p></p>
+          </>
+            )
+         }
 }
-
-export default PizzaMenu
-
-
-
-
-
-
-// export class PizzaMenu extends Component {
-
-//   render() {
-//     // const [pizzas, setPizzas] = useState([])
-//     // const [isLoading, setIsLoading] = useState(false)
-
-//     // const fetchPizzas = () => {
-//     //   setIsLoading(true)
-
-//     //   fetch('https://pizzaria-miguel.herokuapp.com/api/pizzas/index')
-//     //     .then(response => {
-//     //       return response.json()
-//     //     })
-//     //     .then (data => {
-//     //       setIsLoading(false)
-//     //       setPizzas(data)
-//     //     })
-//     // }
-
-
-//     // useEffect(()=> {
-//     //   fetchPizzas()
-//     // }, ['https://pizzaria-miguel.herokuapp.com/api/pizzas/index'])
-
-//   let pizzaFetch = fetch('https://pizzaria-miguel.herokuapp.com/api/pizzas/index')
-//   .then((response) => response.json())
-//   .then((response) => {JSON.parse(response)})
-//   .then((data) => {console.log(data)})
-
-//   .catch((error) => (console.log ("error: "+ error)))
-
-//       return(
-//         <div>
-//           {}//no error
-
-//         </div>
-//            )
-
-//   }
-// }
-
-// export default PizzaMenu;
+         export default PizzaMenu
