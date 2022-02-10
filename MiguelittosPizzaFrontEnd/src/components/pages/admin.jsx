619a2f83f69f10b900d0349be9e23da76b358ae2
MartinO55 <cas022103@coderacademy.edu.au>
2022-02-10 15:53:22 +1100

    Update admin.jsx

--- MiguelittosPizzaFrontEnd/src/components/pages/admin.jsx
+++ MiguelittosPizzaFrontEnd/src/components/pages/admin.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 
-
 const Admin = () => {
-    return 
-}
+	return (
+		<img
+			src="https://st.depositphotos.com/1003814/5052/i/950/depositphotos_50523105-stock-photo-pizza-with-tomatoes.jpg"
+			alt="pizza"
+		/>
+	);
+};
 
-export default Admin
+export default Admin;
