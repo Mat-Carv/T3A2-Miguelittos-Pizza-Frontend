fe58840212d3c75014f7c3243050200a0eacfc39
Martin Olson <martinguyolson@gmail.com>
2022-02-10 17:39:15 +1100

    added comments to app file explaining issue with capitalisation. Now the error is gone. IT KNOWSgit status

--- MiguelittosPizzaFrontEnd/src/App.jsx
+++ MiguelittosPizzaFrontEnd/src/App.jsx
@@ -6,8 +6,8 @@ import useLocalStorage from "use-local-storage"
 import Context from './components/context/context.jsx'
 
   import PizzaMenu from './components/pages/PizzaMenu'
-  import DIYPizza from './components/pages/DIYPizza'
-  import Admin from './components/pages/Admin.jsx'
+  import DIYPizza from './components/pages/DIYPizza' //this import occasionally throws an error where it shows as the wrong page, even though it isn't, and it loads regardless
+  import Admin from './components/pages/Admin.jsx' //this import occasionally throws an error where it shows as the wrong page, even though it isn't, and it loads regardless
   import PlaceOrder from './components/pages/PlaceOrder'
   import SignUp from './components/pages/SignUp'
   import LandingPage from './components/pages/LandingPage.jsx' //this import occasionally throws an error where it shows as the wrong page, even though it isn't, and it loads regardless
