68c2cc33a5e83b5be189db4bc2029f6c67505ef4
Martin Olson <martinguyolson@gmail.com>
2022-02-12 12:57:01 +1100

    styling for pending orders mostly finished

--- MiguelittosPizzaFrontEnd/src/components/pages/PendingOrders.jsx
+++ MiguelittosPizzaFrontEnd/src/components/pages/PendingOrders.jsx
@@ -4,12 +4,13 @@ import Title from "../Header/Header";
 import apiUrl from "../../config/api";
 
 const PendingOrders = () =>{
-
+    //Url locations
     const summonOrdersURL = '/api/admin/orders/index'
-
+    const putOrdersURL = '/api/admin/orders/'
+    //state for saving order data from server
     const [orders, setOrders] = useState([])
 
-
+    //get the orders from the server
     useEffect(async () => {
         console.log("Orders fetched")
           try{
@@ -18,14 +19,14 @@ const PendingOrders = () =>{
             setOrders(data)
           }
           catch(error){
-              console.error(error + " in menu fetch")
+              console.error(error + " in order fetch")
           }
       }, [])
-
+      //refresh the page to reload the orders
       function refreshPage(){
           window.location.reload(false);
       }
-        
+    //function to mark order as complete on the server and refresh the page
     const MarkOrderComplete = () => {
         
         const orderIDASecondTime = orders.map((newOrder, orderIndex) => {
@@ -35,7 +36,7 @@ const PendingOrders = () =>{
         const payload = {
             "complete": true
         }
-            apiUrl.put('/api/admin/orders/'+orderIDASecondTime, payload)
+            apiUrl.put(putOrdersURL+orderIDASecondTime, payload)
             .then(function (response) {
                 if(response.status === 201)
                 {
@@ -49,64 +50,72 @@ const PendingOrders = () =>{
                 console.log(error);
             });   
       }
-
+//function to return each order
    function returnOrders(){ 
        
        const EachOrder = orders.map((newOrder, orderIndex) => {
         
             return(
                 <>
-                <div>
-                    <div id ='order'>
-                        <div>
-                            <div>
-                            Order ID: {newOrder.id}
+                <div id='OuterCard' className="card border-dark mb-3"style={{width: 33 +'em'}} >
+                    <div id ='wholecard'>
+                        <div id='everything but the button'>
+                            <div id='header' className="card-header">
+                                <div id='orderIDnumber'>
+                                Order ID: {newOrder.id}
+                                </div>
+                                <div id='orderName'>
+                                Name: {newOrder.name}
+                                </div>
                             </div>
-                            <div>
-                            Name: {newOrder.name}
-                            </div>
-                            <div>{newOrder.pizza.map((aPizza,idx) =>(
-        
-                                <><></><div>
-                                    Pizzas Type: {aPizza.name}
-                                </div><div>
-                                        Pizza base: {aPizza.base}
-                                    </div><div>
-                                        Pizza Sauce: {aPizza.sauce}
-                                    </div><div>
-                                        Pizza Toppings:
-                                        <div>{Object.keys(aPizza.toppings).map(function (key) {
-                                            var topping = aPizza.toppings[key];
-                                            return (
-                                                <div>
-                                                    {key}: {topping}
+                            <div id="BodyContent" className="card-body text-dark">
+                                <div id='EachPizza'>{newOrder.pizza.map((aPizza,idx) =>(
+                                    // return each pizza in the order
+                                    <><div id="pizzaName" className="card-title">
+                                        <p>Pizzas Type: {aPizza.name}</p>
+                                    </div>
+                                    <div id="aboutApizza" className="card-text">
+                                        <div id="pizzabase" >
+                                               <p>Pizza base: {aPizza.base}</p>
+                                        </div>
+                                        <div id="Pizza Sauce">
+                                               <p>Pizza Sauce: {aPizza.sauce}</p>
+                                        </div>
+                                        <div id ='pizzaToppings'>
+                                               <p> Pizza Toppings:</p>
+                                                <div id="justCode">{Object.keys(aPizza.toppings).map(function (key) {
+                                                    var topping = aPizza.toppings[key];
+                                                    return (//return each topping on the pizza
+                                                        <div id="eachTopping">
+                                                         <p>{key}: {topping}</p> 
+                                                        </div>
+                                                    );
+                                                }
+                                                )}
                                                 </div>
-                                            );
-                                        }
-                                        )}</div>
-
-                                    </div></>
-                                ))
-                            }
-                            </div>
-                            <div>
-                            Drinks: {newOrder.drink}
-                            </div>
-                            <div>
-                            Sides: {newOrder.side}
+                                        </div>
+                                    </div>
+                                        </>
+                                    ))
+                                }
+                                </div>
+                                <div id="drinks">
+                                Drinks: {newOrder.drink}
+                                </div>
+                                <div id="sides">
+                                Sides: {newOrder.side}
+                                </div>
                             </div>
                         </div>
-                        <div>
-                            <button onClick={MarkOrderComplete}>Mark Complete</button>
+                        <div id="markCompleteButton">
+                            <button onClick={MarkOrderComplete} className='btn btn-success'>Mark Complete</button>
                         </div>
                     </div>
                 </div>
                 </>
             )
     })
-            return( EachOrder)
-                
-            
+            return(EachOrder) 
     }
 
 
